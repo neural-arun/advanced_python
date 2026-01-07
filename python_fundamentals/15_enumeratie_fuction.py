e53f496060d78ad1d8fc31f0045fbdb38c9d026b
Arun Yadav <arunyadav149413@gmail.com>
2026-01-07 21:10:37 +0530

    did a lamda function excercise

--- python_fundamentals/15_enumeratie_fuction.py
+++ python_fundamentals/15_enumeratie_fuction.py
@@ -3,5 +3,5 @@ family = ["Arun","Sarita","Uday","Sunny","Ayush"]
 for counter,member in enumerate(family):
     print(counter,member)
 
-for counter,member in enumerate(family, start=1):
+for counter,member in enumerate(family, start=1):  #This starts counter from 1.
     print(counter,member)
---
+++ python_fundamentals/20_excercise.py
@@ -0,0 +1,6 @@
+def over_age(data, getter):
+    return getter(data) >= 18
+
+user = { 'username': 'rolf123', 'age': '35' }
+
+print(over_age(user, lambda x: int(x['age'])))
