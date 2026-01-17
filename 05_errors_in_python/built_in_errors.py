2ba04d0a210b630119d3f284bd3c88ded63c1d0f
Arun Yadav <arunyadav149413@gmail.com>
2026-01-17 09:05:42 +0530

    added built in errors

---
+++ 05_errors_in_python/built_in_errors.py
@@ -0,0 +1,18 @@
+"""
+1- IndexError: when we enter index out of range ( most of the indexing start from 0.)
+2- KeyError: when no is found.
+3- NameError: when the variable is not defined.
+4- AttributeError: list object has no attribute "intersection"
+5- NotImplementedError: some thing wrong in the code.
+6- RuntimeError: 
+7 - SyntaxError:
+8- IndentationError: 
+9- TabError: when we use both tab and spaces together.
+10- TypeError: 5 + "hi"
+11- valueError: 
+12- ImportError:
+13:DeprecationWarning:
+
+and infinite custom made own error.
+
+"""
