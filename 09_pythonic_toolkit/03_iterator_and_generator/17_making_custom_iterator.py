66d157e7a1bdd1fab612d2f65cbd07cb9216db4f
Arun Yadav <arunyadav149413@gmail.com>
2026-01-22 19:51:58 +0530

    restructured some directories

---
+++ 09_pythonic_toolkit/03_iterator_and_generator/17_making_custom_iterator.py
@@ -0,0 +1,14 @@
+class Counter:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.limit:
+            raise StopIteration
+        value = self.current
+        self.current += 1
+        return value
